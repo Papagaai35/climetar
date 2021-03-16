d4474f34dc618282044d3b35a92f151f7728d12c
Daan Gommers <dj.gommers@mindef.nl>
2021-03-16 09:47:52 +0100

    Adds Astro to __init__

--- climetar/__init__.py
+++ climetar/__init__.py
@@ -4,5 +4,7 @@ from .metar import *
 from .stationrepo import StationRepo
 from .metartheme import MetarTheme
 from .metarfile import MetarFiles, import_files
+from .planetary import Astro
 
 from .metarplot import MetarPlotter
+
