8626e71fa64bb89aa075b26486d7a69c85ce6d80
Daan Gommers <dj.gommers@mindef.nl>
2021-04-26 14:38:15 +0200

    Fixes bad key text.kerning_factor warnings

--- climetar/__init__.py
+++ climetar/__init__.py
@@ -1,3 +1,12 @@
+
+#Prevents the 'Bad Key "text.kerning_factor" on line 4' warning when working with matplotlib
+with open('/opt/conda/lib/python3.7/site-packages/matplotlib/mpl-data/stylelib/_classic_test_patch.mplstyle','r+') as fh:
+    lines = fh.readlines()
+    lines = ['#'+line for line in lines if line.startswith("text.kerning_factor")]
+    fh.seek(0)
+    fh.writelines(lines)
+    fh.truncate()
+
 from .quantities import *
 from .metar import *
 
